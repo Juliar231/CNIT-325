d6d55ba752d748a71ffd5349b9e5e253e7c41282
lily-graham1 <lily.grahamster@gmail.com>
2025-04-19 15:24:28 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,1 +1,21 @@
+public class Player {
+    private String name;
+    private int coins;
 
+    public Player(String name) {
+        this.name = name;
+        this.coins = 0;
+    }
+
+    public void earnCoins(int amount) {
+        coins += amount;
+    }
+
+    public int getCoins() {
+        return coins;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
