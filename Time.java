c59de40c1249916ea0bc045d3270e071b73ac01e
lily-graham1 <lily.grahamster@gmail.com>
2025-04-19 15:16:36 -0400

    Update Time.java

--- Time.java
+++ Time.java
@@ -1,1 +1,23 @@
+import java.util.*;
 
+public class Time {
+    private long startTime;
+    private long endTime;
+
+    public void startTime() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public void stopTime() {
+        endTime = System.currentTimeMillis();
+    }
+
+    public void resetTime() {
+        startTime = 0;
+        endTime = 0;
+    }
+
+    public long getGameDuration() {
+        return endTime - startTime;
+    }
+}
