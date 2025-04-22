70f9bd1b722fcbb549cc526621ce74ecfd8cca0c
csakakib <chisato72013@gmail.com>
2025-04-22 16:49:38 -0400

    Create AircraftCarrier.java

---
+++ AircraftCarrier.java
@@ -0,0 +1,9 @@
+public class AircraftCarrier extends Ship {
+    public AircraftCarrier() {
+        super(5, "Aircraft Carrier");
+    }
+    
+    public boolean launchAircraft() {
+        return true;
+    }
+}
