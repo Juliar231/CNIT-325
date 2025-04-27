16da6cecc7f53a2f0a8ce8b1e73196426b923f8e
mbojars17 <michaelbojarski17@gmail.com>
2025-04-27 17:29:29 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,21 +1,52 @@
+package battleshipgame;
+
+
 public class Player {
     private String name;
+    private Board defenseBoard;
+    private AttackBoard attackBoard;
     private int coins;
-
+    
     public Player(String name) {
         this.name = name;
+        this.defenseBoard = new Board();
+        this.attackBoard = new AttackBoard();
         this.coins = 0;
     }
-
-    public void earnCoins(int amount) {
-        coins += amount;
+    
+    public void placeShips() {
+        defenseBoard.placeShip(new Submarine(), 0, 0, true);
+        defenseBoard.placeShip(new Destroyer(), 2, 2, false);
+        defenseBoard.placeShip(new Ferry(), 4, 3, true);
+        defenseBoard.placeShip(new AircraftCarrier(), 1, 1, false);
     }
-
+    
+    public boolean attack(Player opponent, int x, int y) {
+        boolean hit = attackBoard.attack(x, y, opponent.defenseBoard);
+        if (hit) {
+            coins += 10;
+        }
+        return hit;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
     public int getCoins() {
         return coins;
     }
-
-    public String getName() {
-        return name;
+    
+    public Board getDefenseBoard() {
+        return defenseBoard;
+    }
+    
+    public boolean hasLost() {
+        for (Ship ship : defenseBoard.getShips()) {
+            if (!ship.isSunk()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
