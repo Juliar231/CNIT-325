4755fdbfb97b826399d43a296d23861f4b4fa555
csakakib <chisato72013@gmail.com>
2025-05-02 16:26:38 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,52 +1,56 @@
-package battleshipgame;
+package battleshipdemo;
 
-
+/**
+ * Player class to handle player data and actions
+ */
 public class Player {
     private String name;
-    private Board defenseBoard;
+    private DefenseBoard defenseBoard;
     private AttackBoard attackBoard;
     private int coins;
     
     public Player(String name) {
         this.name = name;
-        this.defenseBoard = new Board();
+        this.defenseBoard = new DefenseBoard();
         this.attackBoard = new AttackBoard();
         this.coins = 0;
-    }
-    
-    public void placeShips() {
-        defenseBoard.placeShip(new Submarine(), 0, 0, true);
-        defenseBoard.placeShip(new Destroyer(), 2, 2, false);
-        defenseBoard.placeShip(new Ferry(), 4, 3, true);
-        defenseBoard.placeShip(new AircraftCarrier(), 1, 1, false);
-    }
-    
-    public boolean attack(Player opponent, int x, int y) {
-        boolean hit = attackBoard.attack(x, y, opponent.defenseBoard);
-        if (hit) {
-            coins += 10;
-        }
-        return hit;
     }
     
     public String getName() {
         return name;
     }
     
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public DefenseBoard getDefenseBoard() {
+        return defenseBoard;
+    }
+    
+    public AttackBoard getAttackBoard() {
+        return attackBoard;
+    }
+    
     public int getCoins() {
         return coins;
     }
     
-    public Board getDefenseBoard() {
-        return defenseBoard;
+    public void earnCoins(int amount) {
+        this.coins += amount;
     }
     
-    public boolean hasLost() {
-        for (Ship ship : defenseBoard.getShips()) {
-            if (!ship.isSunk()) {
-                return false;
+    public boolean fireShot(Player opponent, int row, int col) {
+        boolean hit = opponent.getDefenseBoard().receiveAttack(row, col);
+        if (hit) {
+            attackBoard.markHit(row, col);
+            Ship hitShip = opponent.getDefenseBoard().getShipAt(row, col);
+            if (hitShip != null && hitShip.isSunk()) {
+                earnCoins(hitShip.getLength() * 10); // Reward for sinking a ship
             }
+        } else {
+            attackBoard.markMiss(row, col);
         }
-        return true;
+        return hit;
     }
 }
