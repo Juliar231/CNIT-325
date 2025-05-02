2244c0ef4cb5b387d45a291cad3480a2fede9e05
csakakib <chisato72013@gmail.com>
2025-05-02 16:27:21 -0400

    Update Submarine.java

--- Submarine.java
+++ Submarine.java
@@ -1,6 +1,13 @@
-package battleshipgame;
+package battleshipdemo;
 
+/**
+ * Submarine ship class - 2 units length
+ */
 public class Submarine extends Ship {
+    
+    /**
+     * Creates a new Submarine
+     */
     public Submarine() {
         super("Submarine", 2);
     }
