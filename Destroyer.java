4c7706e532c7f895c9c03b90946b6240b4524003
csakakib <chisato72013@gmail.com>
2025-05-02 16:25:51 -0400

    Update Destroyer.java

--- Destroyer.java
+++ Destroyer.java
@@ -1,7 +1,13 @@
-package battleshipgame;
+package battleshipdemo;
 
-
+/**
+ * Destroyer ship class - 3 units length
+ */
 public class Destroyer extends Ship {
+    
+    /**
+     * Creates a new Destroyer
+     */
     public Destroyer() {
         super("Destroyer", 3);
     }
