0648cb814714e2c5e212217e8ce87af930213d1c
csakakib <chisato72013@gmail.com>
2025-05-02 16:26:03 -0400

    Update Ferry.java

--- Ferry.java
+++ Ferry.java
@@ -1,8 +1,14 @@
-package battleshipgame;
+package battleshipdemo;
 
+/**
+ * Ferry ship class - 4 units length
+ */
 public class Ferry extends Ship {
+    
+    /**
+     * Creates a new Ferry
+     */
     public Ferry() {
         super("Ferry", 4);
     }
 }
-
