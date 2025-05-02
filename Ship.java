bd9d5bcb205bf36c6e971fb4aa41e6343e063880
csakakib <chisato72013@gmail.com>
2025-05-02 16:27:06 -0400

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -1,30 +1,60 @@
-package battleshipgame;
+package battleshipdemo;
 
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Abstract Ship class that provides base functionality for all ship types
+ */
 public abstract class Ship {
-    protected int size;
-    protected int hits;
+    protected int length;
+    protected int health;
     protected String name;
-    
-    public Ship(String name, int size) {
+    protected boolean isSunk;
+    protected List<int[]> coordinates;
+
+    public Ship(String name, int length) {
         this.name = name;
-        this.size = size;
-        this.hits = 0;
+        this.length = length;
+        this.health = length;
+        this.isSunk = false;
+        this.coordinates = new ArrayList<>();
     }
-    
+
+    public boolean isHit(int row, int col) {
+        for (int[] coord : coordinates) {
+            if (coord[0] == row && coord[1] == col) {
+                health--;
+                if (health <= 0) {
+                    isSunk = true;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
     public boolean isSunk() {
-        return hits >= size;
+        return isSunk;
     }
-    
-    public void hit() {
-        hits++;
-    }
-    
+
     public String getName() {
         return name;
     }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setCoordinates(List<int[]> coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public List<int[]> getCoordinates() {
+        return coordinates;
+    }
     
-    public int getSize() {
-        return size;
+    public int getHealth(){
+        return health;
     }
 }
