e9a61f9802991c3f7d505725566d7525c89227bf
csakakib <chisato72013@gmail.com>
2025-05-02 16:25:18 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,60 +1,31 @@
-package battleshipgame;
+package battleshipdemo;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Board {
-    protected static final int SIZE = 10;
+/**
+ * Abstract Board class that is extended by AttackBoard and DefenseBoard
+ */
+public abstract class Board {
+    protected static final int BOARD_SIZE = 10;
     protected char[][] grid;
-    protected List<Ship> ships;
     
     public Board() {
-        grid = new char[SIZE][SIZE];
-        ships = new ArrayList<>();
-        initializeGrid();
-    }
-    
-    private void initializeGrid() {
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                grid[i][j] = '~';
+        grid = new char[BOARD_SIZE][BOARD_SIZE];
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                grid[i][j] = '~'; // Water
             }
         }
     }
     
-    public boolean placeShip(Ship ship, int x, int y, boolean horizontal) {
-        // Check boundaries
-        if (x < 0 || y < 0 || x >= SIZE || y >= SIZE) {
-            return false;
+    public char getCell(int row, int col) {
+        if (row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE) {
+            return grid[row][col];
         }
-        
-        // Check if ship fits
-        if (horizontal) {
-            if (y + ship.getSize() > SIZE) return false;
-            for (int i = 0; i < ship.getSize(); i++) {
-                if (grid[x][y + i] != '~') return false;
-            }
-        } else {
-            if (x + ship.getSize() > SIZE) return false;
-            for (int i = 0; i < ship.getSize(); i++) {
-                if (grid[x + i][y] != '~') return false;
-            }
-        }
-        
-        // Place the ship
-        for (int i = 0; i < ship.getSize(); i++) {
-            if (horizontal) {
-                grid[x][y + i] = 'S';
-            } else {
-                grid[x + i][y] = 'S';
-            }
-        }
-        ships.add(ship);
-        return true;
+        return ' '; // Out of bounds
     }
     
-    public List<Ship> getShips() {
-        return ships;
+    public void setCell(int row, int col, char value) {
+        if (row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE) {
+            grid[row][col] = value;
+        }
     }
 }
